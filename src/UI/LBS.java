431fc2613df4ef2c5edf45b6a8b5ee6cafa1406e
MahiaVP <sunnix035@gmail.com>
2026-05-20 11:57:26 -0500

    UI

---
+++ src/UI/LBS.java
@@ -0,0 +1,11 @@
+package UI;
+
+import javax.swing.*;
+
+public class LBS {
+    private JLabel TEXT;
+    private JLabel TEXT2;
+    private JButton Location;
+    private JButton Add;
+    private JButton Lend;
+}
