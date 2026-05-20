d9acd5658d820a37291f34b9242a5f7c2780f022
MahiaVP <sunnix035@gmail.com>
2026-05-20 11:55:25 -0500

    DB connection

---
+++ src/DB/DataConnection.java
@@ -0,0 +1,14 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+public class DataConnection {
+    private static final String KEY="npg_Pi15oQsUerOB";
+    private static final String USER="neondb?";
+    private static final String URL="jdbc:postgresql://ep-dawn-feather-aphaq3sf-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channelBinding=require";
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL,KEY,USER);
+    }
+}
